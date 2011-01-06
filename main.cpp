788cce10096bed1d3f9a4899847c1bf2b5120e6e
Cihangir Akturk <cihangir@bilkon-kontrol.com.tr>
2011-01-06 16:02:03 +0200

    Add logging feature to project.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
 #include <QtGui/QApplication>
 #include "widget.h"
 
+#ifdef LOGGING
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+ofstream logfile;
+void myMessageHandler(QtMsgType type, const char *msg)
+{
+    switch (type) {
+    case QtDebugMsg:
+        logfile << QTime::currentTime().toString("HH:mm-ss").toAscii().data()
+                << " Debug: " << msg << '\n';
+        break;
+    case QtCriticalMsg:
+        logfile << QTime::currentTime().toString("HH:mm-ss").toAscii().data()
+                << " Critical: " << msg << '\n';
+        break;
+    case QtWarningMsg:
+        logfile << QTime::currentTime().toString("HH:mm-ss").toAscii().data()
+                << " Warning: " << msg << '\n';
+        break;
+    case QtFatalMsg:
+        logfile << QTime::currentTime().toString("HH:mm-ss").toAscii().data()
+                << " Fatal: " << msg << '\n';
+        abort();
+    }
+}
+#endif
+
 int main(int argc, char *argv[])
 {
+#ifdef LOGGING
+    logfile.open("/home/cihangir/logfile.txt", ios::app);
+    qInstallMsgHandler(myMessageHandler);
+#endif
     QApplication a(argc, argv);
     Widget w;
     w.show();
